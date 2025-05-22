605860e1e78696559b2ad3b6f43f83f7021e81e6
swap357 <swapnilpatel357@gmail.com>
2025-05-21 18:39:36 -0700

    revert llama3.py to original implementation

--- llama3.py
+++ llama3.py
@@ -28,16 +28,14 @@ def silu(x):
     return x * (1 / (1 + np.exp(-x)))
 
 
-def compute_cos_sin_cache(
-    head_dim: int, max_seq_len: int, base: int = 10000, dtype=np.float32
-):
+def compute_cos_sin_cache(head_dim: int, max_seq_len: int, base: int = 10000):
     inv_freq: Array[HD // 2] = 1.0 / (
         base ** (np.arange(0, head_dim, 2)[: (head_dim // 2)] / head_dim)
     )
     t: Array[M] = np.arange(max_seq_len)
     freqs: Array[M, HD // 2] = np.outer(t, inv_freq)
 
-    return np.cos(freqs).astype(dtype), np.sin(freqs).astype(dtype)
+    return np.cos(freqs), np.sin(freqs)
 
 
 def apply_rotary_emb(
@@ -91,33 +89,29 @@ class FeedForward:
         up_weight: Array[FD, D],
         gate_weight: Array[FD, D],
         down_weight: Array[D, FD],
-        dtype=np.float32,
     ):
-        self.up_weight = up_weight.T.astype(dtype)
-        self.gate_weight = gate_weight.T.astype(dtype)
-        self.down_weight = down_weight.T.astype(dtype)
-        self.dtype = dtype
+        self.up_weight = up_weight.T
+        self.gate_weight = gate_weight.T
+        self.down_weight = down_weight.T
 
     def __call__(self, x: Array[B, L or 1, D]):
+        # FD = 2 * 4 * D / 3
         swish: Array[B, L or 1, FD] = silu(x @ self.gate_weight)
         x_V: Array[B, L or 1, FD] = x @ self.up_weight
-        x_ff: Array[B, L or 1, FD] = swish * x_V
-        x_out: Array[B, L or 1, D] = x_ff @ self.down_weight
-        return x_out.astype(self.dtype)
+        x: Array[B, L or 1, FD] = swish * x_V
+        x: Array[B, L or 1, D] = x @ self.down_weight
+        return x
 
 
 class RMSNorm:
-    def __init__(self, weight: Array[H], eps: float, dtype=np.float32):
-        self.weight = weight.astype(dtype)
+    def __init__(self, weight: Array[H], eps: float):
+        self.weight = weight
         self.eps = eps
-        self.dtype = dtype
 
     def __call__(self, x: Array[B, L or 1, D]):
-        z_float32: Array[B, L or 1, 1] = (x.astype(np.float32) ** 2).mean(
-            -1, keepdims=True
-        ) + self.eps
-        z: Array[B, L or 1, D] = x / np.sqrt(z_float32.astype(self.dtype))
-        return (z * self.weight).astype(self.dtype)
+        z: Array[B, L or 1, 1] = (x**2).mean(-1, keepdims=True) + self.eps
+        z: Array[B, L or 1, D] = x / np.sqrt(z)
+        return z * self.weight
 
 
 class Attention:
@@ -128,7 +122,6 @@ class Attention:
         v_weight: Array[D, D],
         o_weight: Array[D, D],
         args: ModelArgs,
-        dtype=np.float32,
     ):
         self.n_kv_heads = args.n_heads if args.n_kv_heads is None else args.n_kv_heads
         assert args.n_heads % self.n_kv_heads == 0
@@ -136,12 +129,11 @@ class Attention:
         self.n_local_kv_heads = self.n_kv_heads
         self.n_rep = self.n_local_heads // self.n_local_kv_heads
         self.head_dim = args.dim // args.n_heads
-        self.dtype = dtype
 
-        self.q_weight = q_weight.T.astype(self.dtype)
-        self.k_weight = k_weight.T.astype(self.dtype)
-        self.v_weight = v_weight.T.astype(self.dtype)
-        self.o_weight = o_weight.T.astype(self.dtype)
+        self.q_weight = q_weight.T
+        self.k_weight = k_weight.T
+        self.v_weight = v_weight.T
+        self.o_weight = o_weight.T
 
         self.cache_k = np.zeros(
             (
@@ -149,8 +141,7 @@ class Attention:
                 args.max_seq_len,
                 self.n_local_kv_heads,
                 self.head_dim,
-            ),
-            dtype=self.dtype,
+            )
         )
         self.cache_v = np.zeros(
             (
@@ -158,8 +149,7 @@ class Attention:
                 args.max_seq_len,
                 self.n_local_kv_heads,
                 self.head_dim,
-            ),
-            dtype=self.dtype,
+            )
         )
 
     def __call__(
@@ -179,13 +169,13 @@ class Attention:
 
         xq: Array[B, L or 1, QHN, HD] = xq.reshape(
             B, L, self.n_local_heads, self.head_dim
-        ).astype(self.dtype)
+        )
         xk: Array[B, L or 1, KVHN, HD] = xk.reshape(
             B, L, self.n_local_kv_heads, self.head_dim
-        ).astype(self.dtype)
+        )
         xv: Array[B, L or 1, KVHN, HD] = xv.reshape(
             B, L, self.n_local_kv_heads, self.head_dim
-        ).astype(self.dtype)
+        )
 
         # RoPE #2
         xq, xk = apply_rotary_emb(xq, xk, freqs_cos, freqs_sin)
@@ -207,49 +197,43 @@ class Attention:
 
         # Scaled Dot-Product Attention
         # ["B, HN, L or 1, HD"] @ ["B, HN, HD, L"] -> ["B, HN, L or 1, L"]
-        attention_float32: Array[B, HN, L or 1, L] = (
-            xq.astype(np.float32) @ xk.transpose(0, 1, 3, 2).astype(np.float32)
-        ) / math.sqrt(self.head_dim)
+        attention: Array[B, HN, L or 1, L] = (
+            xq @ xk.transpose(0, 1, 3, 2) / math.sqrt(self.head_dim)
+        )
+        # `mask` is used only once at the beginning.
         if mask is not None:
-            attention_float32 = attention_float32 + mask[None, None, :, :].astype(
-                np.float32
-            )
-        attention = softmax(attention_float32).astype(self.dtype)
+            attention = attention + mask[None, None, :, :]
+        attention = softmax(attention)
         output: Array[B, HN, L or 1, HD] = attention @ xv
 
         # ["B, HN, L or 1, HD"] -> ["B, L or 1, D"]
         output: Array[B, L or 1, D] = output.transpose(0, 2, 1, 3).reshape(B, L, -1)
         output: Array[B, L or 1, D] = output @ self.o_weight
 
-        return output.astype(self.dtype)
+        return output
 
 
 class TransformerBlock:
     def __init__(self, weight: dict, layer_id: int, args: ModelArgs):
-        self.dtype = getattr(np, args.dtype)
         self.attention = Attention(
             weight.get(f"model.layers.{layer_id}.self_attn.q_proj.weight"),
             weight.get(f"model.layers.{layer_id}.self_attn.k_proj.weight"),
             weight.get(f"model.layers.{layer_id}.self_attn.v_proj.weight"),
             weight.get(f"model.layers.{layer_id}.self_attn.o_proj.weight"),
             args,
-            dtype=self.dtype,
         )
         self.feed_forward = FeedForward(
             weight.get(f"model.layers.{layer_id}.mlp.up_proj.weight"),
             weight.get(f"model.layers.{layer_id}.mlp.gate_proj.weight"),
             weight.get(f"model.layers.{layer_id}.mlp.down_proj.weight"),
-            dtype=self.dtype,
         )
         self.input_layernorm = RMSNorm(
             weight.get(f"model.layers.{layer_id}.input_layernorm.weight"),
             eps=args.norm_eps,
-            dtype=self.dtype,
         )
         self.post_attention_layernorm = RMSNorm(
             weight.get(f"model.layers.{layer_id}.post_attention_layernorm.weight"),
             eps=args.norm_eps,
-            dtype=self.dtype,
         )
 
     def __call__(
@@ -260,15 +244,19 @@ class TransformerBlock:
         freqs_cos: Array[L or 1, HD // 2],
         freqs_sin: Array[L or 1, HD // 2],
     ):
+        # RMSNorm
         norm_x: Array[B, L or 1, D] = self.input_layernorm(x)
+        # Masked Multi-Head Attention
         h1: Array[B, L or 1, D] = self.attention(
             norm_x, start_pos, mask, freqs_cos, freqs_sin
         )
-        z = (x + h1).astype(self.dtype)
+        z = x + h1
 
+        # RMSNorm
         norm_z = self.post_attention_layernorm(z)
+        # Feed Forward + SwiGLU
         h2: Array[B, L or 1, D] = self.feed_forward(norm_z)
-        out = (z + h2).astype(self.dtype)
+        out = z + h2
 
         return out
 
@@ -276,83 +264,65 @@ class TransformerBlock:
 class Llama:
     def __init__(self, model_path: str, args: ModelArgs):
         self.args = args
-        self.dtype = getattr(np, args.dtype)
 
         weight = load_parameters(model_path)
-        self.tok_embedding: Array[VS, D] = weight.get(
-            "model.embed_tokens.weight"
-        ).astype(self.dtype)
+        self.tok_embedding: Array[VS, D] = weight.get("model.embed_tokens.weight")
 
+        # RoPE #1
         self.freqs_cos, self.freqs_sin = compute_cos_sin_cache(
-            args.dim // args.n_heads, args.max_seq_len, dtype=self.dtype
+            args.dim // args.n_heads, args.max_seq_len
         )
 
         self.layers = []
         for layer_id in range(args.n_layers):
             self.layers.append(TransformerBlock(weight, layer_id, args))
 
-        self.norm = RMSNorm(
-            weight.get("model.norm.weight"), eps=args.norm_eps, dtype=self.dtype
-        )
-        self.lm_head_weight: Array[D, VS] = weight.get("lm_head.weight").T.astype(
-            self.dtype
-        )
+        self.norm = RMSNorm(weight.get("model.norm.weight"), eps=args.norm_eps)
+        self.lm_head_weight: Array[D, VS] = weight.get("lm_head.weight").T
 
         del weight
 
     def __call__(self, input_ids: Array[B, L], start_pos: int):
         _, L = input_ids.shape
-        h: Array[B, L or 1, D] = self.tok_embedding[input_ids].astype(self.dtype)
+        h: Array[B, L or 1, D] = self.tok_embedding[input_ids]
+        # ["M, HD//2"] -> ["L or 1, HD//2"]
         freqs_cos: Array[L or 1, HD // 2] = self.freqs_cos[start_pos : start_pos + L]
         freqs_sin: Array[L or 1, HD // 2] = self.freqs_sin[start_pos : start_pos + L]
 
+        # `mask` is generated only once at the beginning.
         mask: Array[L, L] = None
         if L > 1:
-            mask = np.full((L, L), float("-inf"), dtype=self.dtype)
+            mask = np.full((L, L), float("-inf"))
             mask = np.triu(mask, k=1)
-            zeros_shape = (L, start_pos)
-            if self.dtype is np.float16:
-                mask = np.concatenate(
-                    [np.zeros(zeros_shape, dtype=self.dtype), mask], axis=1
-                )
-            else:
-                mask = np.concatenate([np.zeros(zeros_shape), mask], axis=1).astype(
-                    self.dtype
-                )
+            mask = np.concatenate([np.zeros((L, start_pos)), mask], axis=1)
 
+        # Transformer Layers
         for i, layer in enumerate(self.layers):
             h: Array[B, L or 1, D] = layer(h, start_pos, mask, freqs_cos, freqs_sin)
 
+        # RMSNorm
         h: Array[B, L or 1, D] = self.norm(h)
-        logit: Array[B, 1, VS] = (
-            h[:, [-1], :] @ self.lm_head_weight.astype(np.float32)
-        ).astype(np.float32)
+        # Only forward the output from the last position.
+        # ["B, 1, VS"] = ["B, 1(L), D"] @ ["D, VS"]
+        logit: Array[B, 1, VS] = h[:, [-1], :] @ self.lm_head_weight
         return logit
 
     def generate(self, input_ids: Array[B, L], max_new_tokens: int):
-        B, L_prompt = input_ids.shape
-        curr_L = L_prompt
-        for i in range(max_new_tokens):
-            curr_pos = L_prompt + i
+        _, L = input_ids.shape
+        for i, curr_pos in enumerate(range(L, max_new_tokens)):
             if i == 0:  # Prefill Phase
-                current_input_ids = input_ids
+                inputs = input_ids
                 pos = 0
             else:  # Decode Phase
-                current_input_ids = next_id
-                pos = curr_pos - 1
-
-            logits: Array[B, 1, VS] = self(current_input_ids, pos)
-            next_id = logits[:, -1, :].argmax(-1, keepdims=True).astype(np.int32)
+                inputs = next_id
+                pos = curr_pos
+            logits: Array[B, 1, VS] = self(inputs, pos)
+            next_id = logits[:, -1, :].argmax(-1, keepdims=True)
             yield next_id
-            curr_L += 1
-            if curr_L >= self.args.max_seq_len:
-                break
 
 
 if __name__ == "__main__":
     args = ModelArgs()
-
-    print(f"Using precision: {args.dtype}")
 
     tokenizer = Tokenizer("./tokenizer.model.np")
     model = Llama("./stories15M.model.npz", args)
@@ -366,11 +336,9 @@ if __name__ == "__main__":
     input_ids = np.array([tokenizer.encode(prompt)])
     start = time.time()
     _, L = input_ids.shape
-    generated_tokens_count = 0
-    for id_val in model.generate(input_ids, args.max_new_tokens):
+    for id in model.generate(input_ids, args.max_new_tokens):
         L += 1
-        generated_tokens_count += 1
-        output_id = id_val[0].tolist()
+        output_id = id[0].tolist()
         if output_id[-1] in [tokenizer.eos_id, tokenizer.bos_id]:
             break
         print(tokenizer.decode(output_id), end="")
